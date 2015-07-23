e0c2d3c5c803858709b68d9054ff3271a681b96d
Roman Studer <roman.studer@ergon.ch>
2015-07-23 14:26:30 +0200

    update docker setup

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM sphereio/predictionio
 
 ADD RecommendationEngine /RecommendationEngine
 
+COPY lib/sbt-launch-0.13.8.jar /PredictionIO-0.9.3/sbt/sbt-launch-0.13.8.jar
+
+COPY project/build.properties /PredictionIO-0.9.3/project/build.properties
+
 EXPOSE 8000
 
 ADD run.sh /run.sh
--- run.sh
+++ run.sh
@@ -2,7 +2,27 @@
 
 set -e
 
+echo start PredictionIO
 pio-start-all
+
+echo create app lunchletter
+pio app new lunchletter
+KEY=$(pio app list | grep lunchletter | awk '{print $7}')
+echo "APP KEY=$KEY"
+
 cd /RecommendationEngine
+
+echo "Importing sample data..."
+python data/import_eventserver.py --access_key $KEY
+
+
+echo start build
 pio build --verbose
+
+echo train
+pio train
+
+echo deploy
 pio deploy
+
+echo "done."
